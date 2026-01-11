77d82a3a8526286395fd39194a87b0c96655cb61
Anvayavardhana Kaundinyayana <acharyaanvaya@gmail.com>
2026-01-11 14:37:28 +0545

    css done

---
+++ index.js
@@ -0,0 +1,27 @@
+let hcount = 0
+let gcount = 0
+
+function hinc1(){
+    hcount+=1
+}
+
+function hinc2(){
+    hcount+=2
+}
+
+function hinc3(){
+    hcount+=3
+}
+
+
+function ginc1(){
+    gcount+=1
+}
+
+function ginc2(){
+    gcount+=2
+}
+
+function hinc3(){
+    gcount+=3
+}
