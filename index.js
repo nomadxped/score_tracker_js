3c0b24fcb1959caeddd84a3e713bf54cfc9a9cca
Anvayavardhana Kaundinyayana <acharyaanvaya@gmail.com>
2026-01-11 14:43:38 +0545

    javascript done

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-let hcount = 0
-let gcount = 0
+let hcount = 0;
+let gcount = 0;
 
 function hinc1(){
     hcount+=1
+    document.getElementById("homescore").textContent = hcount
 }
 
 function hinc2(){
     hcount+=2
+    document.getElementById("homescore").textContent = hcount
 }
 
 function hinc3(){
     hcount+=3
+    document.getElementById("homescore").textContent = hcount
 }
 
 
 function ginc1(){
     gcount+=1
+    document.getElementById("guestscore").textContent = gcount
 }
 
 function ginc2(){
     gcount+=2
+    document.getElementById("guestscore").textContent = gcount
 }
 
-function hinc3(){
+function ginc3(){
     gcount+=3
+    document.getElementById("guestscore").textContent = gcount
 }
